ba53eabfdabdbdeb6d928c995a7f882318f4cbf1
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-02-10 02:02:23 +0900

    C언어 pointer(포인터) 연습!(9)

--- C_pointer_9.c
+++ C_pointer_9.c
@@ -6,7 +6,7 @@ int main(void)
 	char* a = name;
 
 	printf("이름을 입력하세요. : ");
-	scanf_s("%s", name, sizeof(name)); //이름 입력받기
+	scanf_s("%s", name, sizeof(name)); // 이름 입력받기
 	
 	printf("당신의 이름은 %s입니다.\n", a);
 
